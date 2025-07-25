b79431308c5a87891dcef81651be4168b7b199d1
Gaurav Matolia <matoliagaurav923@gmail.com>
2025-07-26 01:58:10 +0530

    widgets

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Center(child: Text("Catalog App")),
+        ),
+      body: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to Flutter- Day $days")),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
